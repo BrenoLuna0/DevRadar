31a2f104b9b44c2922008be50580626ba9e196d8
Suporte 03 <breno.luna_@hotmail.com>
2020-01-14 15:14:05 -0300

    Api finalizada

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://breno:1928374655@cluster0-jf7fw.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+});
+
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333);
