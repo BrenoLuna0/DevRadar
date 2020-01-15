f1a5804d1346ce55befa3bf905dc7e76835005ce
Suporte 03 <breno.luna_@hotmail.com>
2020-01-15 14:55:46 -0300

    Criacao do front-end web utilizando react

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://breno:1928374655@cluster0-jf7fw.mongodb.net/week
     useUnifiedTopology : true
 });
 
+app.use(cors({origin : 'http://localhost:3000'}));
 app.use(express.json());
 app.use(routes);
 
